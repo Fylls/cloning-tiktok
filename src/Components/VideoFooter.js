185f2b76626b4dd65832ff841e1b5b79fa22e45f
Fylls <sergentifilippo@gmail.com>
2020-08-20 19:41:56 +0200

    tik-tok video component and snap-scroling feature

---
+++ src/Components/VideoFooter.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./VideoFooter.css"
+
+function VideoFooter() {
+  return (
+    <div className="video_footer">
+      <h2>yye</h2>
+    </div>
+  )
+}
+
+export default VideoFooter
